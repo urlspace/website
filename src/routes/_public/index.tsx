c2ab684d8fdb6d8f8c7664de23a6ad623a0877e8
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2026-05-08 15:25:31 +0100

    unimportant change on the placeholder component on home to test things out

--- src/routes/_public/index.tsx
+++ src/routes/_public/index.tsx
@@ -1,4 +1,4 @@
-import { ButtonLink } from "#/components";
+import { ButtonLink, Stack } from "#/components";
 import { Intro } from "#/sections";
 import { createFileRoute } from "@tanstack/react-router";
 
@@ -13,19 +13,21 @@ function App() {
 
       <h2 className="section__title">What people say</h2>
       <section className="feedback">
-        {Array.from({ length: 6 }).map((_, i) => (
+        {Array.from({ length: 10 }).map((_, i) => (
           // biome-ignore lint/suspicious/noArrayIndexKey: static placeholder list
           <article key={i}>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde id
-              perspiciatis aliquid ipsum vel iusto tenetur nisi est hic error
-              iure earum aspernatur incidunt nihil consequuntur et qui, at
-              itaque?
-            </p>
-            <div>
-              <span>Some name here</span>
-              <span>Some role here</span>
-            </div>
+            <Stack>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde id
+                perspiciatis aliquid ipsum vel iusto tenetur nisi est hic error
+                iure earum aspernatur incidunt nihil consequuntur et qui, at
+                itaque?
+              </p>
+              <div>
+                <span>Some name here</span>
+                <span>Some role here</span>
+              </div>
+            </Stack>
           </article>
         ))}
       </section>
