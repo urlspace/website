e8fb001f1927065e825e646e76806f36c4479ab6
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2026-05-08 15:25:04 +0100

    simplification on the intro component/secion

--- src/sections/Intro/Intro.tsx
+++ src/sections/Intro/Intro.tsx
@@ -1,4 +1,4 @@
-import { ButtonLink } from "#/components";
+import { ButtonLink, Heading, Stack } from "#/components";
 import "./Intro.css";
 
 function Intro() {
@@ -6,55 +6,55 @@ function Intro() {
     <section className="intro">
       <div className="intro__banner">
         <div className="intro__content">
-          <div className="intro__title">
-            <h1 className="intro__heading">url.space</h1>
-          </div>
+          <Stack>
+            <Heading text="url.space" level={1} />
+            <p className="intro__description">
+              Keep, organise and share websites you like. Open source, no ads,
+              no tracking, no AI, just a space for your URLs. Free for everyday
+              use with power user features for a tiny fee.
+            </p>
+            <ButtonLink text="Sign up" to="/auth/signup" />
+          </Stack>
+        </div>
+      </div>
+      <div className="intro__feature">
+        <Stack>
+          <Heading text="Organise" level={2} />
           <p className="intro__description">
-            Keep, organise and share websites you like. Open source, no ads, no
-            tracking, no AI, just a space for your URLs. Free for everyday use
-            with power user features for a tiny fee.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit
+            consectetur, culpa velit consequatur nihil repellat est harum
+            possimus nemo fugiat iste perspiciatis sapiente laudantium! Veniam.
           </p>
-          <ButtonLink text="Sign up" to="/auth/signup" />
-        </div>
+          <a href="#" className="intro__more">
+            Organisation with ease
+          </a>
+        </Stack>
       </div>
       <div className="intro__feature">
-        <div className="intro__title">
-          <h2 className="intro__heading">Organise</h2>
-        </div>
-        <p className="intro__description">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit
-          consectetur, culpa velit consequatur nihil repellat est harum possimus
-          nemo fugiat iste perspiciatis sapiente laudantium! Veniam.
-        </p>
-        <a href="#" className="intro__more">
-          Organisation with ease
-        </a>
+        <Stack>
+          <Heading text="Share" level={2} />
+          <p className="intro__description">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit
+            consectetur, culpa velit consequatur nihil repellat est harum
+            possimus nemo fugiat iste perspiciatis sapiente laudantium! Veniam.
+          </p>
+          <a href="#" className="intro__more">
+            Lean more about sharing
+          </a>
+        </Stack>
       </div>
       <div className="intro__feature">
-        <div className="intro__title">
-          <h2 className="intro__heading">Share</h2>
-        </div>
-        <p className="intro__description">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit
-          consectetur, culpa velit consequatur nihil repellat est harum possimus
-          nemo fugiat iste perspiciatis sapiente laudantium! Veniam.
-        </p>
-        <a href="#" className="intro__more">
-          Lean more about sharing
-        </a>
-      </div>
-      <div className="intro__feature">
-        <div className="intro__title">
-          <h2 className="intro__heading">Use everywhere</h2>
-        </div>
-        <p className="intro__description">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit
-          consectetur, culpa velit consequatur nihil repellat est harum possimus
-          nemo fugiat iste perspiciatis sapiente laudantium! Veniam.
-        </p>
-        <a href="#" className="intro__more">
-          Explore integrations
-        </a>
+        <Stack>
+          <Heading text="Use everywhere" level={2} />
+          <p className="intro__description">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit
+            consectetur, culpa velit consequatur nihil repellat est harum
+            possimus nemo fugiat iste perspiciatis sapiente laudantium! Veniam.
+          </p>
+          <a href="#" className="intro__more">
+            Explore integrations
+          </a>
+        </Stack>
       </div>
     </section>
   );
