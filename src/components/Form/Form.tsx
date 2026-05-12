0c8a95c3382d528a04fc5007f8171995ee876746
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2026-05-12 07:21:42 +0100

    the "required" part of the input label should be hidden from the screen readers

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,95 +1,95 @@
 import "./Form.css";
 
 function Form({
-	onSubmit,
-	isLoading,
-	children,
+  onSubmit,
+  isLoading,
+  children,
 }: {
-	onSubmit: React.ComponentProps<"form">["onSubmit"];
-	isLoading?: boolean;
-	children: React.ReactNode;
+  onSubmit: React.ComponentProps<"form">["onSubmit"];
+  isLoading?: boolean;
+  children: React.ReactNode;
 }) {
-	return (
-		<form
-			aria-busy={isLoading}
-			className="form"
-			onSubmit={onSubmit}
-			tabIndex={-1}
-		>
-			{children}
-		</form>
-	);
+  return (
+    <form
+      aria-busy={isLoading}
+      className="form"
+      onSubmit={onSubmit}
+      tabIndex={-1}
+    >
+      {children}
+    </form>
+  );
 }
 
 function Input({
-	autoComplete,
-	description,
-	disabled,
-	label,
-	maxLength,
-	minLength,
-	name,
-	onChange,
-	pattern,
-	placeholder,
-	required,
-	type,
-	value,
+  autoComplete,
+  description,
+  disabled,
+  label,
+  maxLength,
+  minLength,
+  name,
+  onChange,
+  pattern,
+  placeholder,
+  required,
+  type,
+  value,
 }: {
-	autoComplete?: string;
-	description?: string;
-	disabled?: boolean;
-	label: string;
-	maxLength?: number;
-	minLength?: number;
-	name: string;
-	onChange: (value: string) => void;
-	pattern?: string;
-	placeholder: string;
-	required: boolean;
-	type: "text" | "email" | "password";
-	value: string;
+  autoComplete?: string;
+  description?: string;
+  disabled?: boolean;
+  label: string;
+  maxLength?: number;
+  minLength?: number;
+  name: string;
+  onChange: (value: string) => void;
+  pattern?: string;
+  placeholder: string;
+  required: boolean;
+  type: "text" | "email" | "password";
+  value: string;
 }) {
-	return (
-		<div className="form__field">
-			<label className="form__label" htmlFor={name}>
-				{label}
-				{required ? " (required)" : null}
-			</label>
+  return (
+    <div className="form__field">
+      <label className="form__label" htmlFor={name}>
+        {label}
+        {required ? <span aria-hidden="true"> (required)</span> : null}
+      </label>
 
-			<input
-				aria-describedby={
-					description ? `${name}-description form-error` : "form-error"
-				}
-				autoComplete={autoComplete}
-				className="form__input"
-				disabled={disabled}
-				id={name}
-				maxLength={maxLength}
-				minLength={minLength}
-				name={name}
-				onChange={(e) => onChange(e.target.value)}
-				pattern={pattern}
-				placeholder={placeholder}
-				required={required}
-				type={type}
-				value={value}
-			/>
-			{description ? (
-				<p id={`${name}-description`} className="form__description">
-					{description}
-				</p>
-			) : null}
-		</div>
-	);
+      <input
+        aria-describedby={
+          description ? `${name}-description form-error` : "form-error"
+        }
+        autoComplete={autoComplete}
+        className="form__input"
+        disabled={disabled}
+        id={name}
+        maxLength={maxLength}
+        minLength={minLength}
+        name={name}
+        onChange={(e) => onChange(e.target.value)}
+        pattern={pattern}
+        placeholder={placeholder}
+        required={required}
+        type={type}
+        value={value}
+      />
+      {description ? (
+        <p id={`${name}-description`} className="form__description">
+          {description}
+        </p>
+      ) : null}
+    </div>
+  );
 }
 
 function FormError({ errorMessage }: { errorMessage: string | null }) {
-	return errorMessage ? (
-		<p id="form-error" className="form__error" role="alert">
-			{errorMessage}
-		</p>
-	) : null;
+  return errorMessage ? (
+    <p id="form-error" className="form__error" role="alert">
+      {errorMessage}
+    </p>
+  ) : null;
 }
 
 Form.Input = Input;
