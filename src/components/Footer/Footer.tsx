d52a1686d90e194ec0d099800d476d7a95fd234f
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2026-05-16 05:27:23 +0100

    changes to the footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,9 @@
 import "./Footer.css";
 
 export default function Footer() {
-  return <footer className="footer">footer</footer>;
+  return (
+    <footer className="footer">
+      © 2026 Copyright url.space. All rights reserved.
+    </footer>
+  );
 }
