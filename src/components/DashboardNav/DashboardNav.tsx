78ba99304e1beaf7ac4aa719ab65ac8fa42a798d
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2026-05-22 17:58:56 +0100

    Show collections/tags only when at least one exists

--- src/components/DashboardNav/DashboardNav.tsx
+++ src/components/DashboardNav/DashboardNav.tsx
@@ -32,12 +32,10 @@ function DashboardNav({
   setForLater,
   setSelectedCollection,
   setSelectedTags,
-  setIsAddLinkOpen,
   collections,
   tags,
   selectedCollection,
   selectedTags,
-  setIsAddCollectionOpen,
   handleSignOut,
   showUserSection,
 }: {
@@ -48,12 +46,10 @@ function DashboardNav({
   setForLater: React.Dispatch<React.SetStateAction<boolean>>;
   setSelectedCollection: React.Dispatch<React.SetStateAction<string | null>>;
   setSelectedTags: React.Dispatch<React.SetStateAction<string[]>>;
-  setIsAddLinkOpen: React.Dispatch<React.SetStateAction<boolean>>;
   collections: CollectionRow[];
   tags: TagRow[];
   selectedCollection: string | null;
   selectedTags: string[];
-  setIsAddCollectionOpen: React.Dispatch<React.SetStateAction<boolean>>;
   handleSignOut: () => void;
   showUserSection: boolean;
 }) {
@@ -92,65 +88,70 @@ function DashboardNav({
           </DashboardList.Li>
         </DashboardList>
       </DashboardSection>
-      <DashboardSection>
-        <DashboardAccordion summary="Collections">
-          <Stack>
-            <DashboardList>
-              {collections.map((c) => (
-                <DashboardList.Li key={c.id}>
-                  <DashboardButton
-                    icon={<Icon.Folder />}
-                    onClick={() =>
-                      setSelectedCollection((prev) =>
-                        prev === c.id ? null : c.id,
-                      )
-                    }
-                    ariaPressed={selectedCollection === c.id}
-                    text={c.name}
-                  />
-                </DashboardList.Li>
-              ))}
-            </DashboardList>
-            <DashboardButton
-              icon={<Icon.Edit />}
-              onClick={() => alert("Show filters")}
-              text="Edit collections"
-            />
-          </Stack>
-        </DashboardAccordion>
-      </DashboardSection>
-      <DashboardSection>
-        <DashboardAccordion summary="Tags">
-          <Stack>
-            <DashboardList>
-              {tags.map((t) => (
-                <DashboardList.Li key={t.id}>
-                  <DashboardButton
-                    icon={<Icon.Tag />}
-                    onClick={() =>
-                      setSelectedTags((prev) =>
-                        prev.includes(t.id)
-                          ? prev.filter((id) => id !== t.id)
-                          : [...prev, t.id],
-                      )
-                    }
-                    ariaPressed={selectedTags.includes(t.id)}
-                    text={t.name}
-                  />
-                </DashboardList.Li>
-              ))}
-            </DashboardList>
-
-            <div>
+      {collectoins.length ? (
+        <DashboardSection>
+          <DashboardAccordion summary="Collections">
+            <Stack>
+              <DashboardList>
+                {collections.map((c) => (
+                  <DashboardList.Li key={c.id}>
+                    <DashboardButton
+                      icon={<Icon.Folder />}
+                      onClick={() =>
+                        setSelectedCollection((prev) =>
+                          prev === c.id ? null : c.id,
+                        )
+                      }
+                      ariaPressed={selectedCollection === c.id}
+                      text={c.name}
+                    />
+                  </DashboardList.Li>
+                ))}
+              </DashboardList>
               <DashboardButton
                 icon={<Icon.Edit />}
-                text="Edit tags"
                 onClick={() => alert("Show filters")}
+                text="Edit collections"
               />
-            </div>
-          </Stack>
-        </DashboardAccordion>
-      </DashboardSection>
+            </Stack>
+          </DashboardAccordion>
+        </DashboardSection>
+      ) : null}
+      {tags.length ? (
+        <DashboardSection>
+          <DashboardAccordion summary="Tags">
+            <Stack>
+              <DashboardList>
+                {tags.map((t) => (
+                  <DashboardList.Li key={t.id}>
+                    <DashboardButton
+                      icon={<Icon.Tag />}
+                      onClick={() =>
+                        setSelectedTags((prev) =>
+                          prev.includes(t.id)
+                            ? prev.filter((id) => id !== t.id)
+                            : [...prev, t.id],
+                        )
+                      }
+                      ariaPressed={selectedTags.includes(t.id)}
+                      text={t.name}
+                    />
+                  </DashboardList.Li>
+                ))}
+              </DashboardList>
+
+              <div>
+                <DashboardButton
+                  icon={<Icon.Edit />}
+                  text="Edit tags"
+                  onClick={() => alert("Show filters")}
+                />
+              </div>
+            </Stack>
+          </DashboardAccordion>
+        </DashboardSection>
+      ) : null}
+
       {showUserSection ? (
         <DashboardSection>
           <DashboardList>
